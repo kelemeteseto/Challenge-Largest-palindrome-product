a65ab8db7499513d9509b5a16e46893b4e029d5d
oteswang <oteswang@shackmbp.local>
2015-06-17 18:38:09 -1000

    updates to Challenge-Largest-palindrome-product

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -10,10 +10,22 @@
 module.exports = function(multiplicands, digits){
   var factor_0 = 0;
   var factor_1 = 0;
-  var palindromeNumber = 0;
+  var palindromeNumber = -Infinity;
+
   // do your work here
+  var maxValue = Math.pow(10, digits);
 
-
+  for(var x = 0; x < maxValue; x++) {
+    for(var y = 0; y < maxValue; y++) {
+      var product = x * y;
+      // if(product <= maxValue) break;
+      if(product.toString() === product.toString().split('').reverse().join('') && product > palindromeNumber) {
+        factor_0 = x;
+        factor_1 = y;
+        palindromeNumber = product;
+      }
+    }
+  }
 
   return {
     factor_0 : factor_0,
